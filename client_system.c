c888b46c3e40c2146083febca11f2432e24cc227
村岡　拓哉 <c501406033@clpc037.(none)>
2016-02-04 20:00:30 +0900

    touch

--- client_system.c
+++ client_system.c
@@ -155,16 +155,16 @@ int Collision(int clientID,int befx,int befy){
                 if(gClients[i].ADsta==1){//相手が攻撃なら
                     //  if(((gClients[clientID].poi.x+33) - (gClients[i].poi.x+13)) <= 50 && ((gClients[i].poi.x+13) - (gClients[clientID].poi.x+33)) <= 50){//大きめの範囲で
                     //     if(((gClients[clientID].poi.y+57) - (gClients[i].poi.y+37)) <= 50 && ((gClients[i].poi.y+37) - (gClients[clientID].poi.y+50)) <= 50){
-                    /* if(gClients[clientID].poi.x+33 < gClients[i].poi.x+33+gClients[i].poi.w-66+20){
+                    /*if(gClients[clientID].poi.x+33 < gClients[i].poi.x+33+gClients[i].poi.w-66+20){
                         if(gClients[clientID].poi.y+57 < gClients[i].poi.y+57+gClients[i].poi.h-114+20){
                             if(gClients[clientID].poi.x+33 + gClients[clientID].poi.w-66 > gClients[i].poi.x+33-20){
                                 if(gClients[clientID].poi.y+57 + gClients[clientID].poi.h-114 > gClients[i].poi.y+57-20){
                     */
-                    
-                    if(gClients[clientID].poi.x+ Dfx < gClients[i].poi.x+ Atx + Atw){
-                        if(gClients[clientID].poi.x + Dfx +Dfw > gClients[i].poi.x + Atx){
-                            if(gClients[clientID].poi.y + Dfy < gClients[i].poi.y + Aty + Ath){
-                                if(gClients[clientID].poi.y + Dfy + Dfh> gClients[i].poi.y + Aty){
+                                    
+                    if(gClients[clientID].poi.x + Dfx       < gClients[i].poi.x + Atx + Atw){
+                        if(gClients[clientID].poi.x + Dfx + Dfw > gClients[i].poi.x + Atx){
+                            if(gClients[clientID].poi.y + Dfy       < gClients[i].poi.y + Aty + Ath){
+                                if(gClients[clientID].poi.y + Dfy + Dfh > gClients[i].poi.y + Aty){
                                     
                                     if(gClients[clientID].tackle == 0){//攻守反転していて
                                         if(tflag >=1 && tflag <= 10){//自分がタックルしてたら
