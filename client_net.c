7356997cfcbbd3426aed0a746a493203c03b7cfe
坂井　衛 <c501406003@clpc026.(none)>
2016-02-11 20:21:03 +0900

    com

--- client_net.c
+++ client_net.c
@@ -32,58 +32,58 @@ CLIENT gClients[MAX_CLIENTS];
 機能	: サーバーとのコネクションを設立し，
 		  ユーザーの名前の送受信を行う
 引数	: char	*hostName		: ホスト
-		  int	*num			: 全クライアント数
+		  int	*client			: クライアントID
 		  char	clientNames[][]		: 全クライアントのユーザー名
 出力	: コネクションに失敗した時-1,成功した時0
 *****************************************************************/
 int SetUpClient(char *hostName,int *clientID,char clientNames[][MAX_NAME_SIZE])
 {
-
-struct hostent *servHost;
-int	len;
-char	str[16];//name入力
-int     namelen;//(/n)以外の長さ
-int     err;//エラー
-int i=0;
-char *ipad;
-
-/* ホスト名からホスト情報を得る */
-if((servHost = gethostbyname(hostName))==NULL){
-fprintf(stderr,"Unknown host\n");
-return -1;
-}
-
- char hostname[128];
- char sendData[1024];
-  /* ホスト名を取得 */
-  gethostname(hostname, sizeof(hostname));
- 
-  /* 取得したホスト名を出力 */
-  printf("ホスト名: %s\n", hostname);
-
+    
+    struct hostent *servHost;
+    int	len;
+    char	str[16];//name入力
+    int     namelen;//(/n)以外の長さ
+    int     err;//エラー
+    int i=0;
+    char *ipad;
+    
+    /* ホスト名からホスト情報を得る */
+    if((servHost = gethostbyname(hostName))==NULL){
+        fprintf(stderr,"Unknown host\n");
+        return -1;
+    }
+    
+    char hostname[128];
+    char sendData[1024];
+    /* ホスト名を取得 */
+    gethostname(hostname, sizeof(hostname));
+    
+    /*受信用と送信用ソケットの作成*/
     sendsock = socket(AF_INET, SOCK_DGRAM, 0);
     recvsock = socket(AF_INET, SOCK_DGRAM, 0);
     
     send_addr.sin_family = AF_INET;
     send_addr.sin_port = htons(20000);
-    //send_addr.sin_addr.s_addr =hostName;
-
- bcopy(servHost->h_addr,(char*)&send_addr.sin_addr,servHost->h_length);
-        
+    
+    bcopy(servHost->h_addr,(char*)&send_addr.sin_addr,servHost->h_length);
+    
     recv_addr.sin_family = AF_INET;
     recv_addr.sin_port = htons(30000);
     recv_addr.sin_addr.s_addr = INADDR_ANY;
-
+    
     bind(recvsock, (struct sockaddr *)&recv_addr, sizeof(recv_addr));
+    
 
     fprintf(stderr,"connected\n");
+
+    /*名前の入力*/
     do{
         err = 0;//指定文字数以内で入力されるまで繰り返す
         printf("名前を入力してください\n");
         fgets(str, sizeof(str), stdin);//文字列の読み込み
         *strchr(str,'\n') = '\0';//含まれた改行を消す
         namelen = strlen(str);
-
+        
         if(namelen == 0){//なにも入力されてないときのエラーメッセージ
             printf("1文字以上で");
             err=1;
@@ -93,67 +93,61 @@ return -1;
             err = 1;
         }
     }while(err);
-
+    
     InitWindows();
-
+    
+    /*通信ができる状態であることをサーバーに伝えるため「1」を送る*/
     sprintf(sendData,"1\0");
     SendData(sendData);
     printf("sendData = %s\n",sendData);
 
     printf("Please Wait\n");
-    while(i==0){
-    recv(recvsock, buf, sizeof(buf), 0);
-    printf("recvData = %s\n",buf);
-    i=atoi(buf);
-    printf("recvData = %d\n",i);
-    }
 
+    /*クライアントが全員送ったらサーバーから「1」が送られるのでループを抜ける*/
+    while(i==0){
+        recv(recvsock, buf, sizeof(buf), 0);
+
+        i=atoi(buf);
+    }
+    
+    /*サーバーからクライアントの数が送られる*/
     recv(recvsock, buf, sizeof(buf), 0);
     cnum=atoi(buf);
-  
+
+    /*サーバーに名前とホスト名を送る*/
     sprintf(sendData, "%s,%s\0", str, hostname);
-
-    printf("%s\n",sendData);
     SendData(sendData);
-
+    
     printf("Please Wait\n");
+    
 
+    /*サーバーからそれぞれのクライアント名前、ホスト名、IDが送られてくる*/
     for(i=0;i<cnum;i++){
+        recv(recvsock, buf, sizeof(buf), 0);
+        
+        printf("%s\n",buf);
+        
+        char *app_id = strtok(buf, ",");
+        char *name = strtok(NULL, ",");
+        char *host = strtok(NULL, ",");
+        int id=atoi(strtok(NULL, ","));
+        strcpy(gClients[id].name,name);
+        
+        /*ホスト名が自分と同じであればそのクライアントは自分なので、自身のIDを保管する*/
+        if(strcmp(hostname,host)==0)
+            *clientID=id;
+        
+        printf("app_id = %s\n",app_id);
+        printf("name = %s\n",name);
+        printf("host = %s\n",host);
+        printf("id   = %d\n",id);
+        
+    }
+    
+    printf("clientID=%d\n",*clientID);
     recv(recvsock, buf, sizeof(buf), 0);
-
     printf("%s\n",buf);
 
-    char *app_id = strtok(buf, ",");
-    char *name = strtok(NULL, ",");
-    char *host = strtok(NULL, ",");
-    int id=atoi(strtok(NULL, ","));
-    strcpy(gClients[id].name,name);
-
-    if(strcmp(hostname,host)==0)
-       *clientID=id;
-
-printf("app_id = %s\n",app_id);
-printf("name = %s\n",name);
-printf("host = %s\n",host);
-printf("id   = %d\n",id);
-
-    }
-
-        printf("clientID=%d\n",*clientID);
-recv(recvsock, buf, sizeof(buf), 0);
-printf("%s\n",buf);
-
-//recv(recvsock, buf, sizeof(buf), 0);
-//cnum=atoi(buf);
-printf("clientnum=%d\n",cnum);
-
-
-    /* 全クライアントのユーザー名を得る */
-    //GetAllName(clientID,num,clientNames);
-
-    /* select()のためのマスク値を設定する */
-    //SetMask();
-    
     return 0;
 }
 
@@ -167,14 +161,12 @@ printf("clientnum=%d\n",cnum);
 int SendRecvManager(void)
 {
     recv(recvsock, buf, sizeof(buf), 0);
-    printf("buf=%s\n",buf);
-
+    
     int		endFlag = 1;
+    
+    /* コマンドに対する処理を行う */
+    endFlag = ExecuteCommand(buf);
 
-    	/* コマンドに対する処理を行う */
-		endFlag = ExecuteCommand(buf);
-                printf("endflag=%d\n",endFlag);
-    // }
     return endFlag;
 }
 
@@ -182,7 +174,6 @@ int SendRecvManager(void)
 関数名	: SendData
 機能	: サーバーにデータを送る
 引数	: void		*data		: 送るデータ
-		  int		dataSize	: 送るデータのサイズ
 出力	: なし
 *****************************************************************/
 void SendData(char *data)
@@ -195,9 +186,9 @@ void SendData(char *data)
         }
         length++;
     }
-
+    
     sendto(sendsock,data,length,0,(struct sockaddr *)&send_addr, sizeof(send_addr));
-
+    
 }
 
 /*****************************************************************
